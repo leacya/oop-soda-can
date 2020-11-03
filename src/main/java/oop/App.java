263e540abb3c7b59fd5b499e2d59fec262b2bddb
annel <anne.leacy@dkit.ie>
2020-11-03 20:06:08 +0000

    Implemented the getters and setters for SodaCan Called the getters and setters from App

--- src/main/java/oop/App.java
+++ src/main/java/oop/App.java
@@ -17,6 +17,16 @@ public class App
         SodaCan sc1 = new SodaCan(8,5);
         //TODO
         // See question P8.5
+        System.out.println("Height of sodacan = " + sc1.getHeight());
+        System.out.println("Radius of sodacan = " + sc1.getRadius());
+
+        sc1.setHeight(10);
+        sc1.setRadius(3);
+
+        System.out.println("Changed values of instance variables as follows:");
+        System.out.println("Height of sodacan = " + sc1.getHeight());
+        System.out.println("Radius of sodacan = " + sc1.getRadius());
+
 
 
     }
--- src/main/java/oop/SodaCan.java
+++ src/main/java/oop/SodaCan.java
@@ -15,4 +15,23 @@ public class SodaCan
         this.radius = radius;
     }
 
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(double height)
+    {
+        this.height = height;
+    }
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+    }
 }
